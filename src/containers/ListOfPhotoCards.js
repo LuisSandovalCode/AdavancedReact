40366e3f1cee4177932c37fbca72b312cdbe7cce
Luis Sandoval <sandoval.luis2097@gmail.com>
2019-12-15 20:23:24 -0600

    Container para el fetching de datos

---
+++ src/containers/ListOfPhotoCards.js
@@ -0,0 +1,3 @@
+import { withPhotos } from "../components/hoc/withPhotos";
+import { ListOfPhotoCardsComponent } from "../components/ListOfPhotoCards";
+export const ListOfPhotoCards = withPhotos(ListOfPhotoCardsComponent);
